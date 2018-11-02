df4129dcc6ca48d96ea1e7c353b206a5964769e9
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-11-02 18:04:25 -0400

    update 11-2

--- liri.js
+++ liri.js
@@ -4,17 +4,19 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+console.log(process.argv);
 
 spotify.search({
-     type: 'track', 
-     query: 'All the Small Things'
+     type: 'track',
+     query: process.argv[2]
 }, function (err, data) {
     if (err) {
         return console.log('Error occurred: ' + err);
     }
 var songs = data.tracks.items;
- for (var i=0; 1<songs.length; i++) {
-  console.log(songs[i].name);   
- }
+ for (var i=0; i<songs.length; i++) {
+  console.log(songs[i].name); 
     
+ }
+    console.log(data);
 });
