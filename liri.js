e22232965a4ee26d082085717d8aa60ca40fe9d0
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-11-06 01:08:51 -0500

    11-5 updates

--- liri.js
+++ liri.js
@@ -2,21 +2,42 @@ require("dotenv").config();
 
 
 var Spotify = require('node-spotify-api');
+var bandsintown = require('bandsintown')(APP_ID);
 var keys = require("./keys.js");
+
+
 var spotify = new Spotify(keys.spotify);
 console.log(process.argv);
 
-spotify.search({
-     type: 'track',
-     query: process.argv[2]
-}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-var songs = data.tracks.items;
- for (var i=0; i<songs.length; i++) {
-  console.log(songs[i].name); 
-    
- }
-    console.log(data);
-});
+var getArtistNames = function (artist) {
+    return artist.name;
+}
+
+var getSpotifySongs = function (songName) {
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+            console.log(i);
+            console.log('artist(s): ' + songs[i] artists map(getArtistNames));
+            console.log('song name: ' + songs[i] name);
+            console.log('preview song: ' + songs[i] preview_url);
+            console.log('album: ' + songs[i] album name);
+            console.log('-----------------------------------')
+
+        }
+        console.log(data);
+    });
+}
+
+bandsintown.getArtistEventList('Skrillex')
+    .then(function (events) {
+        // return array of events
+    });
+
