e14516be89d97593428ea5010b81ef85dc6bc951
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-11-13 21:06:25 -0500

    update 11-13

--- liri.js
+++ liri.js
@@ -1,43 +1,104 @@
 require("dotenv").config();
-
-
+var keys = require('./keys');
 var Spotify = require('node-spotify-api');
-var bandsintown = require('bandsintown')(APP_ID);
-var keys = require("./keys.js");
+//added to format table 
+// var cTable = require('console.table');
+var request = require('request');
+var moment = require('moment');
 
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
 
-var spotify = new Spotify(keys.spotify);
-console.log(process.argv);
+if (process.argv[2] == 'concert-this') {
 
-var getArtistNames = function (artist) {
-    return artist.name;
-}
+    var artist = process.argv.slice(3).join(" ")
+    console.log(artist);
 
-var getSpotifySongs = function (songName) {
-    spotify.search({
-        type: 'track',
-        query: songName
-    }, function (err, data) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(queryURL, function (error, response, body) {
+        if (error) console.log(error);
+        var result = JSON.parse(body);
+        // console.log(response);
+        // console.log(body);
+        // console.log(result);
+        for (var i = 0; i<result.length; i++) {
+
+        console.log("Venue name " + result[i].venue.name);
+        console.log("Venue location " + result[i].venue.city);
+        console.log("Date of Event " + moment(result[i].datetime).format("MM/DD/YYYY"));
+        }
+    });
+
+    // Name of the venue
+    // Venue location
+    // Date of the Event (use moment to format this as "MM/DD/YYYY")   
+} else if (process.argv[2] == 'spotify-this-song') {
+
+    var songName = process.argv.slice(3).join(" ");
+
+    if (songName == undefined) {
+        songName = "The sign by Ace of Base";
+    }
+
+    spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
         if (err) {
-            console.log('Error occurred: ' + err);
-            return;
+            return console.log('Error occurred: ' + err);
         }
-        var songs = data.tracks.items;
-        for (var i = 0; i < songs.length; i++) {
-            console.log(i);
-            console.log('artist(s): ' + songs[i] artists map(getArtistNames));
-            console.log('song name: ' + songs[i] name);
-            console.log('preview song: ' + songs[i] preview_url);
-            console.log('album: ' + songs[i] album name);
-            console.log('-----------------------------------')
 
-        }
-        console.log(data);
+    // console.log(data.tracks);
+    for (var i = 0; i < data.tracks.items.length; i++) {
+        // var result = {
+            console.log('artist: ' + data.tracks.items[i].album.artists[0].name,
+            'album_name: ' + data.tracks.items[i].album.name,
+            'song_name: ' + data.tracks.items[i].name,
+            'preview_url: ' + data.tracks.items[i].preview_url);
+        // }
+        // tableArray.push(result);
+    }
     });
+
+    // var tableArray = [];
+
+
+
+    // var table = cTable.getTable(tableArray);
+
+    // console.log(table);
+
+
+    // If no song is provided then your program will default to "The Sign" by Ace of Base.
+} else if (process.argv[2] == 'movie-this') {
+    var movieName = process.argv.slice[3].join(" ");
+
+    if (movieName == undefined) {
+        movieName = "Mr. Nobody";
+    }
+
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=4ba2b104' + process.argv[3], function (error, response, body) {
+
+        var result = JSON.parse(body);
+        console.log("Title :" + result.Title);
+        console.log("Year :" + result.Released);
+        console.log("IMDB Rating :" + result.imdbRating);
+        console.log("Rotten Tomatoes :" + result.Ratings[1].Value);
+        console.log("Country :" + result.Country);
+        console.log("Language :" + result.Language);
+        console.log("Movie Plot :" + result.Plot);
+        console.log("Actors :" + result.Actors);
+
+    });
+
+} else if (process.argv[2] == 'do-what-it-says') {
+    console.log('do what it says')
 }
 
-bandsintown.getArtistEventList('Skrillex')
-    .then(function (events) {
-        // return array of events
-    });
+//  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
 
+//   console.log(data); 
+//   });
